77496463cad2e615309c4bc82bdb37d1eb4abdf2
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-04-19 17:41:57 -0500

    Advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Configure client ssh with puppent
+
+file_line { 'Turn off password':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
+}
+
+file_line { 'Add identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
